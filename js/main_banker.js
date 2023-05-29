3426206e54d6f532cfbedde7cbdb069ef53d780e
adham-elmalawany <117538822+adham-elmalawany@users.noreply.github.com>
2023-05-29 22:59:07 +0300

    Add files via upload

---
+++ js/main_banker.js
@@ -0,0 +1,84 @@
+/* 
+   *********** BANKER LOGIN PAGE ***********
+*/
+(function ($) {
+    "use strict";
+
+    var full_path = "C:/Users/ADHAM/Downloads/SEProject/login/template_banker/homePageBanker.html";
+    var relative_path = "../template_banker/homePageBanker.html";
+    // Assign link to 'relative_path', if it didn't work; replace it with 'full_path' but adjust it according to your project root location
+    var linkTo_BankerHomePage =  relative_path;
+
+     /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(event){
+        var check = true;
+
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+
+        if (check === true) {
+            // If login credintials valid, then go to banker home page.
+            window.location.href = linkTo_BankerHomePage;
+            // Prevent page refresh from submit button.
+            event.preventDefault();
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
